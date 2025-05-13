b2908dda4cc18870bbcdddb3265dd2efe5a5518c
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-05-13 16:03:06 +0200

    Create server.js and set up MongoDB Atlas connection

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Conntect to MongoDB Atlas
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch((err) => console.error('Connection error: ', err));
+
+// Routes
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+});
